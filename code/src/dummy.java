70a38a306810b93f483c1ff243ae56ecbd1a1d2f
UlfNore <ulf.nore@gmail.com>
2011-09-03 13:56:08 +0200

    Hepp!

--- code/src/dummy.java
+++ code/src/dummy.java
@@ -2,4 +2,8 @@ class dummy {
     public static void main(String[] args) {
         System.out.println("Hello World!"); // Display the string.
     }
+    
+    public void hallaVerden(){
+    	System.out.println("Halla!");
+    }
 }
