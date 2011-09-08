8af48e2646bd955dd902530c9e699d675e45d579
ernie <ernie@epicbytes.net>
2011-09-08 22:58:27 +0200

    first work on xml parser. this was harder than I thought.

--- code/src/dummy.java
+++ code/src/dummy.java
@@ -1,3 +1,4 @@
+
 class dummy {
     public static void test(){
     	System.out.println("Test");
@@ -6,6 +7,13 @@ class dummy {
 	public static void main(String[] args) {
         System.out.println("Hello Worlffd!"); // Display the string.
         test();
+        
+        try {
+			new XMLParser().load("http://info.yahoo.com/privacy/w3c/p3p_policy.xml");
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
     }
     
 }
