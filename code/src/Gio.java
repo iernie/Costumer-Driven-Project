4038291b4c06abbe4421dc186ba8a70d86c8234c
Nicholas Gerstle <ngerstle@gmail.com>
2011-09-19 10:32:34 +0200

    -

--- code/src/Gio.java
+++ code/src/Gio.java
@@ -1,17 +1,21 @@
+package src;
+
 
 import java.io.BufferedReader;	//for configuration file functionality
 import java.io.File;			//for configuration file functionality
 import java.io.FileInputStream;	//for configuration file functionality
+import java.io.FileOutputStream;
 import java.io.FileReader;		//for configuration file functionality
 import java.io.IOException;		//for configuration file functionality
 import java.io.InputStream;		//for configuration file functionality
+import java.io.ObjectOutputStream;
 import java.util.Properties;	//for configuration file functionality
 import java.util.logging.*;		//for logger functionality
 import org.apache.commons.cli.*;	//for command line options
 
 public class Gio {
 
-	
+
 	private static Logger logger = Logger.getLogger("");		//create logger object
     	private FileHandler fh; ;					//creates filehandler for logging
 	private String genConfig;					//location of general configuration file
@@ -27,7 +31,7 @@ public class Gio {
 	{
 		// create Options object
 		Options options = new Options();
-	
+
 		// add t option
 		options.addOption("c", true, "general configuration file location");
 		options.addOption("w", true, "weights configuration file location");
@@ -67,7 +71,7 @@ public class Gio {
 	{
 		return loadGeneral(genConfig);
 	}
-	
+
 	/**
 	 * Loads the general config, either from provided string, or default location (./PrivacyAdviser.cfg)
 	 * 
@@ -101,7 +105,7 @@ public class Gio {
 		}	
 		return configFile;
 	}
-	
+
 	/**
 	 * Loads the weights configuration file, from the provided location
 	 * 
@@ -109,7 +113,7 @@ public class Gio {
 	 * @param location of configuration file
 	 * @return properties object corresponding to given configuration file
 	 */
-	
+
 	Properties loadWeights(String fileLoc)
 	{
 		if(wConfig != null)
@@ -141,15 +145,15 @@ public class Gio {
 		}	
 		return configFile;
 	}
-	
-	
+
+
 	/**
 	 * startLogger initializes and returns a file at logLoc with the results of logging at level logLevel. 
 	 * @param logLoc	location of the output log file- a string
 	 * @param logLevel	logging level (is parsed by level.parse())
 	 * @return	Logger object to log to.
 	 */
-	
+
 	Logger startLogger(String logLoc, String logLevel)
 	{
 		try 
@@ -173,8 +177,8 @@ public class Gio {
 		logger.setLevel(Level.parse(logLevel));		//set log level
 		return logger;
 	}
-	
-	
+
+
 	/**
 	 * Loads the case history into cache.
 	 * 
@@ -183,5 +187,23 @@ public class Gio {
 	 */
 	void loadDB()
 	{
+		
+		PolicyObject policyObject = new PolicyObject();// Creating new policy object
+		//adding values to poilicy object.
+		
+		policyObject.addEntityData("1", "Nicholas");
+		policyObject.addEntityData("2", "policy1");
+		policyObject.addEntityData("3", "anyValue");
+		
+
+		FileOutputStream fos = new FileOutputStream("serial1");// creating an output stream to save the object
+		ObjectOutputStream oos = new ObjectOutputStream(fos); // initializing output stream to write policyObject
+		oos.writeObject(policyObject); // writing to output stream policyObject
+		/**
+		 * in similar way depending on number of objects(policies to be created) can be created either by providing
+		 * values hard coded in this class or an interface could be created and this class could be used to take 
+		 * values from the interface and save the data received for the policy
+		 */
+
 	}
 }
