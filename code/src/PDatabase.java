e62fda0a973c0af041924ab01265601266fb0c6c
Nicholas Gerstle <ngerstle@gmail.com>
2011-09-28 13:52:47 +0200

    making PolicyDatabase.java an abstract class, rather than interface as it needs to enforce implementation of iteratable, among other things.

--- code/src/PDatabase.java
+++ code/src/PDatabase.java
@@ -25,34 +25,16 @@ class PDatabase extends PolicyDatabase implements Serializable
 	private  ArrayList<PolicyObject> idb; //internal database
 	public static String location;
 	
-	/**
-	*	Dummy main to test reading and writing.
-	*	
-	*	@param String args[]
-	*	@authors Henrik Knutsen
-	*/
-	/*
-	public void main(String args[]) {
-		/* Initializing testing policies 
-		PolicyLight WritePolicy = new PolicyLight("Henrik", "Admin", 7.5, 20);
-		PolicyLight ReadPolicy = null;
-		
-		/* Writing Policy to disk 
-		writePolicy(WritePolicy);
-		/* Reading data in "Policy.name" to TempPolicy 
-		ReadPolicy = readPolicy(WritePolicy.name);
-		
-		System.out.println(TempPolicy);
-	}*/
-
 
 	/**
-	*	private constructor to ensure singleton-ness
+	*	private constructor to ensure singleton-ness,
+	* deciding what type of collection to use
 	*
 	*	@author Nicholas Gerstle
 	*/
 	private	PDatabase()	
 	{
+		idb = new ArrayList<PolicyObject>();
 	}
 	
 	
@@ -79,102 +61,15 @@ class PDatabase extends PolicyDatabase implements Serializable
 		return i;
 	}	
 	
-	/**
-	*	Public method to write an POLICYLIGHT object to disk
-	*	
-	*	@param The object of type PolicyLight (dummy object) to be written
-	*	@authors Henrik Knutsen, Aman Kaur
-	*/
-	//TO DO unless there's a more efficient way, we don't write individual policies to disk, so remove this method, or change PolicyLight to Policy
-/*	public void writePolicy(PolicyLight Policy) {
-		try {
-			// Creating a stream to create the file "Policy.name" and write bytes to it
-			// Name of the file can be changed to whatever is wanted
-			FileOutputStream fos = new FileOutputStream(Policy.name);
-			// Creating a stream convering object to byte data 
-			ObjectOutputStream oos = new ObjectOutputStream(fos);
-			// Writing datastream of object to file 
-			oos.writeObject(Policy);
-			// Closing streams 
-			fos.close();
-			oos.flush(); 
-			oos.close();
-		} 
-		catch(Exception e) {
-			// Simple error handling, show error and shut down 
-			System.out.println("Exception during serialization: " + e); 
-			System.exit(0); 
-		}
-	} 
-*/
+
+	
+
 
 	
 	/**
-	*	Public method to read POLICY object from disk
-	*	
-	*	@param  Name of the file to be read
-	*	@return Object of type PolicyLight (dummy object), that is read from the file
-	*	@authors Henrik Knutsen, Aman Kaur
-	*/
-	//TO DO unless there's a more efficient way, we don't write individual policies to disk, so remove this method, or change PolicyLight to Policy
-/*	public PolicyLight readPolicy(String filename) {
-		// Initializing the object to be returned 
-		PolicyLight TempPolicy = null;
-		try { 		
-			// Creating a stream to read bytes from file "filename" 
-			FileInputStream fis = new FileInputStream(filename);
-			// Converting bytedata from FileInputStream to an object 
-			ObjectInputStream ois = new ObjectInputStream(fis);
-			// Casting the read object and storing it in return variable 
-			TempPolicy = (PolicyLight)ois.readObject(); 
-			// Closing the streams 
-			fis.close();
-			ois.close();
-		}
-		catch(Exception e) {
-			// Simple error handling, show error and shut down
-			System.out.println("Exception during deserialization: " + e); 
-			System.exit(0); 
-		} 
-		
-		return TempPolicy;
-	}
-*/
-	
-	
-
-
-	/* (non-Javadoc)
-	 * @see PolicyDatabase#getDBIt()
-	 */
-	@Override
-	public Iterator<PolicyObject> getDBIt()
-	{
-		return idb.iterator();
-	}
-
-
-	/* (non-Javadoc)
-	 * @see PolicyDatabase#addPolicy(PolicyObject)
-	 */
-	@Override
-	public void addPolicy(PolicyObject n)
-	{
-		idb.add(n);
-	}
-
-	/* (non-Javadoc)
-	 * @see PolicyDatabase#iterator()
-	 */
-	@Override
-	public Iterator<PolicyObject> iterator() {
-		return idb.iterator();
-	}
-	
-	/* (non-Javadoc)
+	 * See PolicyDatabase.java. implements loading/closing the db via a serialize PDatabase object.
 	 * @see PolicyDatabase#closeDB()
 	 */
-	@Override
 	public void closeDB()
 	{
 		try {
@@ -198,8 +93,10 @@ class PDatabase extends PolicyDatabase implements Serializable
 	}
 	
 	/**
-	 * Loads database from a file dLoc
-	 * 
+	 /**
+	 * See PolicyDatabase.java. implements loading/closing the db via a serialize PDatabase object.
+	 * @see PolicyDatabase#closeDB()
+	 *
 	 * @param dLoc the location of the database file on disk
 	 * 
 	 * @author ngerstle
