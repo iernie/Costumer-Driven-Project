0d2890af48f3f7ff47152e0e70edde0d5cf3b197
Nicholas Gerstle <ngerstle@gmail.com>
2011-11-02 14:12:15 +0100

    fixed conclusion simple (touched action) for confidence. finally

--- code/Couchdb/views/simple/reduce.js
+++ code/Couchdb/views/simple/reduce.js
@@ -1,4 +1,15 @@
-function(keys, values, rereduce) {
-//reduce to nearest neighbors?
-//return acceptance rate of nearest neighbor/s?  
+function(keys, values, rereduce) 
+{
+	//reduce to nearest neighbors
+	//return [[acceptcount,rejectcount,distance,domain,policy],...]
+	var results = [];
+	if(rereduce)
+	{
+		
+	}
+	else
+	{
+		var newpol = 
+	}
+	return results;
 }
