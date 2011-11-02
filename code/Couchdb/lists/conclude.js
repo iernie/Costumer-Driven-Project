7e59ab94afb52a95f1d762a4df63f767d2b3b572
Nicholas Gerstle <ngerstle@gmail.com>
2011-11-02 12:18:12 +0100

    coded Couchdb/view/simple/map, and Couchdb/lists/conclude.js

--- code/Couchdb/lists/conclude.js
+++ code/Couchdb/lists/conclude.js
@@ -1,3 +1,36 @@
 function(head, req) {
-  
+	var action = {"accepted": true, "reasonDomains": [], "override": false, "confidence": 0.0};
+	var accepted = [];
+	var accepted_weight = 0;
+	var rejected = [];
+	var rejected_weight = 0;
+	var i; //an array of [countaccepted, countrejected, distance, domain]
+	while(i=getRow()) 
+	{
+		if(i[0]>0)
+		{
+			accepted.push(i[3]);
+			accepted_weight += (i[2]*i[0]);
+		}
+		if(i[1]>0)
+		{
+			rejected.push(i[3]);
+			rejected_weight += (i[2]*i[0]);
+		}
+	}
+
+	if(accepted_weight < rejected_weight)
+	{
+		action.accepted = true;
+		action.reasonDomains = accepted;
+		action.confidence = (accepted_weight)/(accepted_weight+rejected_weight);
+	}
+	else
+	{
+		action.accepted = false;
+		action.reasonDomains = rejected;
+		action.confidence = (rejected_weight)/(accepted_weight+rejected_weight);
+	}
+		
+	send(JSON.stringify(action));	  
 }
--- code/Couchdb/views/simple/map.js
+++ code/Couchdb/views/simple/map.js
@@ -1,3 +1,7 @@
 function(doc) {
 //compare doc to incomming. return distance, accept, doc ?  
+	if(doc.actionTaken && doc.cases)
+	{
+		emit(doc,doc);
+	}
 }
